ea9a7ab493f3d23fd2551e26c37c5e7f8a523274
Rafael <rafapaz@gmail.com>
2017-12-01 13:20:30 -0200

    Expand range

--- expand.c
+++ expand.c
@@ -3,25 +3,26 @@
 
 #define MAX 9999
 
-char *expande(char *s1, char *s2);
+int expande(char *s1, char *s2);
 
 int main()
 {
 	char s1[MAX], s2[MAX];
 
 	scanf("%s", s1);
-	printf("%s", expande(s1,s2));
+	expande(s1,s2);
+	printf("%s", s2);
 	return 0;
 }
 
-char *expande(char *s1, char *s2)
+int expande(char *s1, char *s2)
 {
 	int i=0,j=0,c=0,inf=0,sup=0,k=0;
 	
 	while (c = s1[i++]) {
 		if (c == '-') {
-			inf = s1[i-1];
-			sup = s1[i+1];
+			inf = s1[i-2]; // because i++
+			sup = s1[i];   // because i++
 		}
 
 		if (sup!=0) {
@@ -32,5 +33,5 @@ char *expande(char *s1, char *s2)
 		}
 	}
 	s2[k] = '\0';
-	return s2;
+	return 0;
 }
