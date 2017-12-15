cd80d26755e04caf2433d10c0ecf4d7d94b6a929
Rafael <rafapaz@gmail.com>
2017-12-15 14:00:16 -0200

    seg fault in strcmp

--- qsort.c
+++ qsort.c
@@ -3,68 +3,39 @@
 #include <string.h>
 #include "util.h"
 
-typedef struct lno {
-	char *word;
-	struct lno *ant;
-	struct lno *prox;
-} No;
+#define MAX 999
+#define LEN 32
 
-No *addLista(No *, char*);
-int compNo(const void *n1, const void *n2);
-void qsortNo(No *base, int start, int end, int (*compar)(const void *, const void *));
+int comp(const void *n1, const void *n2);
 
 int main()
 {
-	int line=0, size=0;
+	int line=0, i=0, size=0;
 	char *w;
-	No *lista = NULL;
-	No *p = NULL;
+	char lista[MAX][LEN];
 
-
-	lista = (No *) malloc(sizeof(No));
-	lista->word = strdup("");
-	lista->ant = lista->prox = NULL;
 	while ((getword(&w, &line)) != EOF) {
-		lista = addLista(lista, w);
-		size++;
+		strcpy(lista[i], w);
+		i++;
 	}
-	lista = lista->prox;
+	size = i;
+	strcpy(lista[i],"\0");
 
 	printf("\nNormal: \n");
-	for (p = lista; p != NULL; p = p->prox)
-		printf("%s\n", p->word);
+	for (i=0;strcmp(lista[i],"\0");i++)
+		printf("%s\n", lista[i]);
 
-	//TODO: Change linked list to array
-	qsort(lista, 0, size-1, compNo);
+	qsort(lista, size, sizeof(char)*MAX*LEN, comp);
 
 	printf("Ordenado: \n");
-	for (p = lista; p != NULL; p = p->prox)
-		printf("%s\n", p->word);
+	for (i=0;strcmp(lista[i],"\0");i++)
+		printf("%s\n", lista[i]);
 	
 	return 0;
 }
 
 
-int compNo(const void *n1, const void *n2)
+int comp(const void *n1, const void *n2)
 {
-	return strcmp(((No*)n1)->word, ((No*)n2)->word);
+	return strcmp((char *)n1,(char *)n2);
 }
-
-No *addLista(No *lista, char *word)
-{
-	No *p;
-	No *new;
-
-	new = (No *) malloc(sizeof(No));
-	new->word = mystrdup(word);
-	new->ant = new->prox = NULL;
-
-	p = lista;
-	while (p->prox != NULL)
-		p = p->prox;
-	p->prox = new;
-	new->ant = p;
-
-	return lista;
-}
-
